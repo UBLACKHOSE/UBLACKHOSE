6cd555bda7bd5698adc2a0e7ab48d194bbbcafbe
UBLACKHOSE <35422221+UBLACKHOSE@users.noreply.github.com>
2020-02-07 23:52:12 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,4 @@
+<?php
+$connection = mysqli_connect('','UBLACKHOSE','admin123');
+$select_db = mysqli_select_db($connection, 	'practice' );
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name= viewport content= width=device-width initial-scale=1>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+require('connect.php');
+
+if (isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query= "INSERT INTO users (username,password,email) VALUE ('$username','$password','$email')";
+    $result = mysqli_query($connection,$query);
+    if($result){
+        $smsg = "Регистрация прошла успешно";
+    } else{
+        $fsmsg="Ошибка";
+    }
+
+}
+
+?>
+<div class="container">
+    <form class="form-signin" method="post">
+        <h2>Registration</h2>
+        <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg;?></div><?php }?>
+        <?php if(isset($fsmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fsmsg;?></div><?php }?>
+        <input type="text" name="username" class="form-control" placeholder="Username" required>
+        <input type="email" name="email" class="form-control" placeholder="Email" required>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+    </form>
+</div>
+</body>
+</html>
