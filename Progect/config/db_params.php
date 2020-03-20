63eca59c8030fcb14fb1ffc43e8a4fde2078db0c
UBLACKHOSE <35422221+UBLACKHOSE@users.noreply.github.com>
2020-03-21 01:05:17 +0700

    123

--- Progect/config/db_params.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'host' => 'localhost',
-    'dbname'=> 'fsite_film',
-    'user' => 'root',
-    'password'=> '',
-)
-?>
--- Progect/views/header_and_footer/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<footer class="pt-4" style="background: #3e4649; height: 40px">
-
-</footer>';
